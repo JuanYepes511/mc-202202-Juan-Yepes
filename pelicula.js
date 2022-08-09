f3efaf879cd57fae8cefbac9873c56f28ec8eaae
JuanYepes511 <104527530+JuanYepes511@users.noreply.github.com>
2022-08-09 06:21:33 -0500

    Add files via upload

---
+++ pelicula.js
@@ -0,0 +1,16 @@
+'use strict'
+var nombre = function(nombre) {
+    return "La pelicula que estas apunto de ver es: " + nombre;
+}
+nombre("Star Wars")
+
+var pelicula = function(nombre) {
+    return "La pelicula que estas apunto de ver es: " + nombre;
+}
+
+function sumar(numero1, numero2) {
+    var suma = numero1 + numero2;
+}
+sumar(3, 5);
+
+document.write("Honestamente la 6 fue la mejor");
