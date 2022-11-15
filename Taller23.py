d266bf619f0f1dd913ece491ecdadcf071c1d685
JuanYepes511 <104527530+JuanYepes511@users.noreply.github.com>
2022-11-14 20:10:56 -0500

    Update Taller23.py

--- Taller23.py
+++ Taller23.py
@@ -10,8 +10,7 @@ x = sym.Symbol('x')
 polinomio = 0
 divisorL = np.zeros(n, dtype = float)
 for i in range(0,n,1):
-    
-    # Termino de Lagrange
+
     numerador = 1
     denominador = 1
     for j  in range(0,n,1):
@@ -23,20 +22,18 @@ for i in range(0,n,1):
     polinomio = polinomio + terminoLi*fi[i]
     divisorL[i] = denominador
 
-# simplifica el polinomio
 polisimple = polinomio.expand()
 
-# para evaluación numérica
+
 px = sym.lambdify(x,polisimple)
 
-# Puntos para la gráfica
 muestras = 101
 a = np.min(xi)
 b = np.max(xi)
 pxi = np.linspace(a,b,muestras)
 pfi = px(pxi)
 
-# SALIDA
+
 print('    valores de fi: ',fi)
 print('divisores en L(i): ',divisorL)
 print()
@@ -46,7 +43,7 @@ print()
 print('Polinomio de Lagrange: ')
 print(polisimple)
 
-# Gráfica
+
 plt.plot(xi,fi,'o', label = 'Puntos')
 plt.plot(pxi,pfi, label = 'Polinomio')
 plt.legend()
